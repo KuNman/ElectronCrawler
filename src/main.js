6790b89e7376a8f5a1ff0071f04815567a97e095
K N <jakub.nejman@ostendi.pl>
2018-10-23 17:01:56 +0200

    Switch to electron-vue

--- src/main.js
+++
@@ -1,9 +0,0 @@
-const electron = require('electron')
-const { app, BrowserWindow } = require('electron')
-
-function createWindow () {
-		win = new BrowserWindow({ width: 800, height: 600 })
-		win.loadFile('./src/views/index.html')
-}
-
-app.on('ready', createWindow)
