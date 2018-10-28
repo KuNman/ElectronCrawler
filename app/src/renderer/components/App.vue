f777d7ea90a928b45cca2d9ede5c8b046c71097f
KuNman <knejman56@gmail.com>
2018-10-28 02:18:50 +0200

    Testing

---
+++ app/src/renderer/components/App.vue
@@ -0,0 +1,16 @@
+<template lang="pug">
+    div
+        ul
+            li 1
+            li 2
+</template>
+
+<script>
+  export default {
+    name: 'App',
+  };
+</script>
+
+<style scoped>
+
+</style>
